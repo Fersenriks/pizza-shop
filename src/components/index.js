078a5364d089ec427f3fbcd39cf12729c74e8ff9
Vladimir Ivanov <vladimir.ivanov1022@gmail.com>
2021-03-29 13:29:24 +0300

    json-server + axios

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,6 @@ import Button from './Button'
 import Header from './Header'
 import Categories from './Categories'
 import Sort from './Sort'
+import PizzaBlock from './PizzaBlock'
 
-export {Button, Header, Categories, Sort}
+export {Button, Header, Categories, Sort, PizzaBlock}
